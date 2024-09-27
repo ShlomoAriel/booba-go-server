abd8ac1e78c98e5cf37bfd6fc09c74e851d46c62
Balink Dev <dev@balink.net>
2024-09-27 06:49:07 -0400

    updated swagger

--- routes/ingredientRoutes.js
+++ routes/ingredientRoutes.js
@@ -3,6 +3,32 @@ const router = express.Router();
 const Ingredient = require('../models/Ingredient');
 
 // Create a new ingredient
+/**
+ * @swagger
+ * /api/ingredients:
+ *   post:
+ *     summary: Create a new ingredient
+ *     tags: [Ingredients]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Sugar"
+ *     responses:
+ *       201:
+ *         description: Ingredient created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Ingredient'
+ *       400:
+ *         description: Bad request
+ */
 router.post('/ingredients', async (req, res) => {
   const { name } = req.body;
   try {
@@ -14,6 +40,45 @@ router.post('/ingredients', async (req, res) => {
   }
 });
 
+// Bulk insert/update ingredients
+/**
+ * @swagger
+ * /api/bulk-ingredients:
+ *   post:
+ *     summary: Bulk insert or update ingredients
+ *     tags: [Ingredients]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: array
+ *             items:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                   example: "Sugar"
+ *     responses:
+ *       201:
+ *         description: Bulk operation was successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 nMatched:
+ *                   type: integer
+ *                   example: 1
+ *                 nUpserted:
+ *                   type: integer
+ *                   example: 1
+ *                 nModified:
+ *                   type: integer
+ *                   example: 1
+ *       500:
+ *         description: Server error
+ */
 router.post('/bulk-ingredients', async (req, res) => {
   const ingredients = req.body;
 
@@ -34,6 +99,24 @@ router.post('/bulk-ingredients', async (req, res) => {
 });
 
 // Get all ingredients
+/**
+ * @swagger
+ * /api/ingredients:
+ *   get:
+ *     summary: Get all ingredients
+ *     tags: [Ingredients]
+ *     responses:
+ *       200:
+ *         description: List of all ingredients
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Ingredient'
+ *       500:
+ *         description: Server error
+ */
 router.get('/ingredients', async (req, res) => {
   try {
     const ingredients = await Ingredient.find();
--- routes/unitRoutes.js
+++ routes/unitRoutes.js
@@ -3,6 +3,32 @@ const router = express.Router();
 const Unit = require('../models/Unit');
 
 // Create a new unit
+/**
+ * @swagger
+ * /api/units:
+ *   post:
+ *     summary: Create a new unit
+ *     tags: [Units]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Kilogram"
+ *     responses:
+ *       201:
+ *         description: Unit created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Unit'
+ *       400:
+ *         description: Bad request
+ */
 router.post('/units', async (req, res) => {
   const { name } = req.body;
   try {
@@ -15,6 +41,24 @@ router.post('/units', async (req, res) => {
 });
 
 // Get all units
+/**
+ * @swagger
+ * /api/units:
+ *   get:
+ *     summary: Get all units
+ *     tags: [Units]
+ *     responses:
+ *       200:
+ *         description: List of all units
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Unit'
+ *       500:
+ *         description: Server error
+ */
 router.get('/units', async (req, res) => {
   try {
     const units = await Unit.find();
