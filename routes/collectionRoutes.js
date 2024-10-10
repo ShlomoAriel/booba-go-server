0e1ae7bf1e2a1471fdbe760d615d07ab8eaa7805
Balink Dev <dev@balink.net>
2024-10-10 08:38:05 -0400

    refactor collectionRoute to support population for all endpoints

--- routes/collectionRoutes.js
+++ routes/collectionRoutes.js
@@ -32,6 +32,56 @@ async function extractMetadata(link) {
   }
 }
 
+// Helper function to populate recipe ingredients, units, and format dates
+async function populateAndFormatCollectionItems(collection) {
+  // Iterate through items and check if it's a Recipe
+  const populatedItems = await Promise.all(
+    collection.items.map(async (item) => {
+      if (item.__t === 'Recipe') {
+        // If the item is a Recipe, populate its ingredients and units
+        const populatedRecipe = await Recipe.populate(item, [
+          {
+            path: 'ingredients.ingredient',
+            select: 'name', // Select specific fields for ingredients
+          },
+          {
+            path: 'ingredients.unit',
+            select: 'name', // Select specific fields for units
+          },
+        ]);
+
+        // Format dates in ISO8601 for Recipe
+        return {
+          ...populatedRecipe.toObject(),
+          createdAt:
+            populatedRecipe.createdAt.toISOString().split('.')[0] + 'Z',
+          updatedAt: populatedRecipe.updatedAt
+            ? populatedRecipe.updatedAt.toISOString().split('.')[0] + 'Z'
+            : undefined, // Optional field for updatedAt
+        };
+      }
+
+      // For non-recipe items, just format the dates
+      return {
+        ...item.toObject(),
+        createdAt: item.createdAt.toISOString().split('.')[0] + 'Z',
+        updatedAt: item.updatedAt
+          ? item.updatedAt.toISOString().split('.')[0] + 'Z'
+          : undefined,
+      };
+    })
+  );
+
+  return {
+    ...collection.toObject(),
+    items: populatedItems,
+    createdAt: collection.createdAt.toISOString().split('.')[0] + 'Z',
+    updatedAt: collection.updatedAt
+      ? collection.updatedAt.toISOString().split('.')[0] + 'Z'
+      : undefined, // Optional field for updatedAt
+  };
+}
+
 /**
  * @swagger
  * /collections:
@@ -120,20 +170,6 @@ router.post('/collections', authenticate, async (req, res) => {
  *               type: array
  *               items:
  *                 type: object
- *                 properties:
- *                   _id:
- *                     type: string
- *                     description: Collection ID
- *                   name:
- *                     type: string
- *                     description: The name of the collection
- *                   items:
- *                     type: array
- *                     items:
- *                       type: string
- *                       description: Collectible item IDs
- *       400:
- *         description: Bad request
  */
 router.get('/collections', authenticate, async (req, res) => {
   try {
@@ -151,59 +187,63 @@ router.get('/collections', authenticate, async (req, res) => {
 
     // Populate ingredients and units in Recipe items and format dates to ISO8601
     const populatedCollections = await Promise.all(
-      collections.map(async (collection) => {
-        // Iterate through items and check if it's a Recipe
-        const populatedItems = await Promise.all(
-          collection.items.map(async (item) => {
-            if (item.__t === 'Recipe') {
-              // If the item is a Recipe, populate its ingredients and units
-              const populatedRecipe = await Recipe.populate(item, [
-                {
-                  path: 'ingredients.ingredient',
-                  select: 'name', // Select specific fields for ingredients
-                },
-                {
-                  path: 'ingredients.unit',
-                  select: 'name', // Select specific fields for units
-                },
-              ]);
-
-              // Format dates in ISO8601 for Recipe
-              return {
-                ...populatedRecipe.toObject(),
-                createdAt:
-                  populatedRecipe.createdAt.toISOString().split('.')[0] + 'Z',
-                updatedAt: populatedRecipe.updatedAt
-                  ? populatedRecipe.updatedAt.toISOString().split('.')[0] + 'Z'
-                  : undefined, // Optional field for updatedAt
-              };
-            }
-
-            // For non-recipe items, just format the dates
-            return {
-              ...item.toObject(),
-              createdAt: item.createdAt.toISOString().split('.')[0] + 'Z',
-              updatedAt: item.updatedAt
-                ? item.updatedAt.toISOString().split('.')[0] + 'Z'
-                : undefined,
-            };
-          })
-        );
-
-        // Return the collection with populated items and formatted dates
-        return {
-          ...collection.toObject(),
-          items: populatedItems,
-          createdAt: collection.createdAt.toISOString().split('.')[0] + 'Z',
-          updatedAt: collection.updatedAt
-            ? collection.updatedAt.toISOString().split('.')[0] + 'Z'
-            : undefined, // Optional field for updatedAt
-        };
-      })
+      collections.map((collection) =>
+        populateAndFormatCollectionItems(collection)
+      )
     );
 
     // Send the populated collections in the response
     res.status(200).json(populatedCollections);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+/**
+ * @swagger
+ * /collections/{id}:
+ *   get:
+ *     summary: Get a collection by ID
+ *     tags:
+ *       - Collections
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the collection
+ *     responses:
+ *       200:
+ *         description: Collection details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       404:
+ *         description: Collection not found
+ *       400:
+ *         description: Bad request
+ */
+router.get('/collections/:id', authenticate, async (req, res) => {
+  try {
+    const collection = await Collection.findById(req.params.id).populate(
+      'items'
+    );
+
+    if (!collection) {
+      return res.status(404).json({ message: 'Collection not found' });
+    }
+
+    // Populate ingredients and units in Recipe items and format dates to ISO8601
+    const populatedCollection = await populateAndFormatCollectionItems(
+      collection
+    );
+
+    // Send the populated collection in the response
+    res.status(200).json(populatedCollection);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -232,8 +272,6 @@ router.get('/collections', authenticate, async (req, res) => {
  *           application/json:
  *             schema:
  *               type: array
- *               items:
- *                 type: object
  */
 router.get('/collectible/search', authenticate, async (req, res) => {
   const query = req.query.search; // Get the search query from the request
@@ -301,268 +339,6 @@ router.get('/collectible/search', authenticate, async (req, res) => {
     res.json(collectibles);
   } catch (error) {
     console.error('Error searching collectibles:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-/**
- * @swagger
- * /collections/{id}:
- *   get:
- *     summary: Get a collection by ID
- *     tags:
- *       - Collections
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         schema:
- *           type: string
- *         description: ID of the collection
- *     responses:
- *       200:
- *         description: Collection details
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 _id:
- *                   type: string
- *                   description: Collection ID
- *                 name:
- *                   type: string
- *                   description: The name of the collection
- *                 items:
- *                   type: array
- *                   items:
- *                     type: string
- *                     description: Collectible item IDs
- *       404:
- *         description: Collection not found
- *       400:
- *         description: Bad request
- */
-router.get('/collections/:id', authenticate, async (req, res) => {
-  try {
-    const collection = await Collection.findById(req.params.id).populate(
-      'items'
-    );
-    if (!collection) {
-      return res.status(404).json({ message: 'Collection not found' });
-    }
-    res.status(200).json(collection);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-/**
- * @swagger
- * /collections/{id}:
- *   put:
- *     summary: Update a collection by ID
- *     tags:
- *       - Collections
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         schema:
- *           type: string
- *         description: ID of the collection to update
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *                 description: The updated name of the collection
- *               items:
- *                 type: array
- *                 items:
- *                   type: string
- *                   description: Updated array of Collectible item IDs
- *     responses:
- *       200:
- *         description: Collection updated successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 _id:
- *                   type: string
- *                   description: Collection ID
- *                 name:
- *                   type: string
- *                   description: The updated name of the collection
- *                 items:
- *                   type: array
- *                   items:
- *                     type: string
- *                     description: Updated array of Collectible item IDs
- *       404:
- *         description: Collection not found
- *       400:
- *         description: Bad request
- */
-router.put('/collections/:id', authenticate, async (req, res) => {
-  try {
-    const { name, items } = req.body;
-
-    const collection = await Collection.findById(req.params.id);
-    if (!collection) {
-      return res.status(404).json({ message: 'Collection not found' });
-    }
-
-    collection.name = name || collection.name;
-    collection.items = items || collection.items;
-
-    await collection.save();
-    res.status(200).json(collection);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-/**
- * @swagger
- * /collections/{id}:
- *   delete:
- *     summary: Delete a collection by ID
- *     tags:
- *       - Collections
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         schema:
- *           type: string
- *         description: ID of the collection to delete
- *     responses:
- *       200:
- *         description: Collection deleted successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: Collection deleted successfully
- *       404:
- *         description: Collection not found
- *       400:
- *         description: Bad request
- */
-router.delete('/collections/:id', authenticate, async (req, res) => {
-  try {
-    const collection = await Collection.findById(req.params.id);
-
-    if (!collection) {
-      return res.status(404).json({ message: 'Collection not found' });
-    }
-
-    await collection.remove();
-    res.status(200).json({ message: 'Collection deleted successfully' });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-/**
- * @swagger
- * /collections/{id}/items:
- *   post:
- *     summary: Add an item to a collection
- *     tags:
- *       - Collections
- *     security:
- *       - BearerAuth: []
- *     parameters:
- *       - name: id
- *         in: path
- *         required: true
- *         schema:
- *           type: string
- *         description: ID of the collection
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               itemId:
- *                 type: string
- *                 description: The ID of the collectible item to add to the collection
- *     responses:
- *       200:
- *         description: Item added to the collection successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 _id:
- *                   type: string
- *                   description: Collection ID
- *                 items:
- *                   type: array
- *                   items:
- *                     type: string
- *                     description: Collectible item IDs
- *       404:
- *         description: Collection not found
- *       400:
- *         description: Bad request
- */
-router.post('/collections/:id/items', authenticate, async (req, res) => {
-  try {
-    const { itemId } = req.body;
-
-    // Validate the itemId
-    if (!itemId) {
-      return res.status(400).json({ message: 'Item ID is required' });
-    }
-
-    // Find the collection by ID
-    const collection = await Collection.findById(req.params.id)
-      .populate('items') // This populates the items field with the actual item documents
-      .exec();
-
-    // If the collection is not found, return 404
-    if (!collection) {
-      return res.status(404).json({ message: 'Collection not found' });
-    }
-
-    // Check if the item already exists in the collection
-    if (collection.items.includes(itemId)) {
-      return res
-        .status(400)
-        .json({ message: 'Item already exists in the collection' });
-    }
-
-    // Add the new item to the collection's items array
-    collection.items.push(itemId);
-
-    // Save the updated collection
-    await collection.save();
-
-    // Respond with the updated collection
-    res.status(200).json(collection);
-  } catch (error) {
-    console.error('Error adding item to collection:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
